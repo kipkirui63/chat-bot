239adc12daccb5c1b72f9d3fe21f5796f654f8f2
kipkirui63 <enocksang8356@gmail.com>
2024-03-14 11:51:03 +0300

    defined used to get response

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import long_responses as long
 
 
 
+# Used to get the response
+def get_response(user_input):
+    split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
+    response = check_all_messages(split_message)
+    return response
+
+
 # Testing the response system
 while True:
     print('Bot: ' + get_response(input('You: ')))
