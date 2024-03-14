f27e8232b8c1e17fd52e39288c552701b7ca8723
kipkirui63 <enocksang8356@gmail.com>
2024-03-14 20:03:16 +0300

    added an html template and responsive page when it  loads

---
+++ __init__.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from main import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_input = request.form['user_input']
+    bot_response = get_response(user_input)
+    return {'bot_response': bot_response}
+
+
+
+if __name__ == '__main__':
+    app.run(port=5555,debug=True)
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import re
 import long_responses as long
 
 
+
+
+
 def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
     message_certainty = 0
     has_required_words = True
@@ -60,6 +63,13 @@ def get_response(user_input):
     return response
 
 
-# Testing the response system
-while True:
-    print('Bot: ' + get_response(input('You: ')))
+
+
+
+    # Testing the response system
+# while True:
+#     print('Bot: ' + get_response(input('You: ')))
+
+
+
+
